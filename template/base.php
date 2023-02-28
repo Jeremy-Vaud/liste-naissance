518b9da1cd98d74bcd5624a84203e28de9be4deb
Jeremy-Vaud <100855862+Jeremy-Vaud@users.noreply.github.com>
2023-02-28 20:31:05 +0100

    header

--- template/base.php
+++ template/base.php
@@ -10,26 +10,28 @@
 </head>
 
 <body class="container mx-auto">
-    <h1 class="text-center text-3xl py-6">Liste de naissance</h1>
-    <div class="flex flex-wrap">
-        <?= $cards ?>
-    </div>
-    <div id="bg-modal" class="fixed w-screen h-screen bg-black opacity-50 top-0 left-0 hidden"></div>
-    <div id="modal" class="fixed w-[300px] bg-white top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] rounded-lg text-center p-6 hidden">
-        <form id="modal-form">
-            <input type="hidden" id="item-id" name="id">
-            <label for="name">Votre nom:</label>
-            <input type="text" id="name" name="name" class="w-full py-1 px-3 mb-6">
-            <button type="submit" class="btn-primary">j'offre ce cadeau</button>
-        </form>
-        <div id="modal-success" class="hidden mb-6">
-            <p class="text-2xl">Merci!</p>
+    <?php include 'template/header.php' ?>
+    <main>
+        <div class="flex flex-wrap">
+            <?= $cards ?>
         </div>
-        <div id="modal-error" class="hidden mb-6">
-            <p>Une erreur est survenue!</p>
+        <div id="bg-modal" class="fixed w-screen h-screen bg-black opacity-50 top-0 left-0 hidden"></div>
+        <div id="modal" class="fixed w-[300px] bg-white top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] rounded-lg text-center p-6 hidden">
+            <form id="modal-form">
+                <input type="hidden" id="item-id" name="id">
+                <label for="name">Votre nom:</label>
+                <input type="text" id="name" name="name" class="w-full py-1 px-3 mb-6">
+                <button type="submit" class="btn-primary">j'offre ce cadeau</button>
+            </form>
+            <div id="modal-success" class="hidden mb-6">
+                <p class="text-2xl">Merci!</p>
+            </div>
+            <div id="modal-error" class="hidden mb-6">
+                <p>Une erreur est survenue!</p>
+            </div>
+            <button id="modal-close" class="text-gray-500 cursor-pointer my-2">Fermer</button>
         </div>
-        <button id="modal-close" class="text-gray-500 cursor-pointer my-2">Fermer</button>
-    </div>
+    </main>
     <script src="dist/main.js"></script>
 </body>
 
